1d91492bfca592d8863ee1423d42f604b6c89ad7
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-13 23:10:34 +0200

    commit-2

--- src/index.js
+++ src/index.js
@@ -53,7 +53,22 @@ function renderCardList(countries) {
   listEl.innerHTML = markup;
 }
 
-function renderCardList() {}
+function renderCardCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <b>${country.name.official}</b></p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+    })
+    .join('');
+  countryEl.innerHTML = markup;
+}
 
 function clearHtml() {
   listEl.innerHTML = '';
