b3144db62f3c84d1a29b23cf4ecdcddf635bd83b
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-02-14 02:16:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,10 +41,8 @@ function onInputSearch(event) {
 function renderCardList(countries) {
   const markup = countries
     .map(country => {
-      return `<li>
-      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
-         <p>${country.name.official}</p>
-                </li>`;
+      return `<img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="40" >
+         <p>${country.name.official}</p>`;
     })
     .join('');
   listEl.innerHTML = markup;
@@ -55,7 +53,7 @@ function renderCardCountry(countries) {
     .map(country => {
       return `<img src="${country.flags.svg}" alt="Flag of ${
         country.name.official
-      }" width="30" hight="20">
+      }" width="40">
          <b>${country.name.official}</b></p>
             <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
